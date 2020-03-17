76140668e34457c080995a52c4b29498a313f379
Liubou Chaika <liubouchaika@gmail.com>
2020-03-17 17:44:47 +0300

    feat: min,max,avg functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array == false || array == undefined) {
+		return 0;
+	} else {
+		let min = array[0];
+		for (let i = 1; i < array.length; i++) {
+			if (min > array[i]) {
+				min = array[i];
+			};
+		};
+		return min;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == false || array == undefined) {
+		return 0;
+	} else {
+		let max = array[0];
+		for (let i = 1; i < array.length; i++) {
+			if (max < array[i]) {
+				max = array[i];
+			};
+		};
+		return max;	
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(array == false || array == undefined) {
+		return 0;
+	} else {
+		let result = array.reduce((sum, current) => sum + current, 0);
+		let avg = result / array.length;
+		return avg;
+	}
 }
